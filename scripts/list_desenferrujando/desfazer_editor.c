934171575882ef225311c254111d327dd90202f4
Luidooo <eng.limaluis@gmail.com>
2023-09-11 13:03:09 -0300

    Lista desenferrujando

---
+++ scripts/list_desenferrujando/desfazer_editor.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    char comando[101];
+    char pedido[101];
+
+    while(scanf(" %s", &comando) !=EOF){
+
+
+
+    }
+    return 0;
+}
--- scripts/list_desenferrujando/viagem_aconselha3.c
+++ scripts/list_desenferrujando/viagem_aconselha3.c
@@ -2,20 +2,24 @@
 
 int main()
 {
-    int r,res,mres=0,mnota=0,nota,dia=0;
+    int r,res,mres=10000001,mnota=0,nota,dia=0;
 
     while(scanf(" %d", &r) != EOF){
         for(int i=0;i < r;i++){
             scanf(" %d %d" , &res, &nota);
-            mres = mres>=res?res:mres;
-            mres = nota == mnota?mres:res;
-            mnota = nota>=mnota?nota:mnota;
-            printf(" %d %d\n", res, nota);
-            printf(" %d %d\n\n", mres, mnota);
+            //mnota = nota>=mnota?nota:mnota;
+            if(nota>mnota){
+                mnota = nota;
+                mres = res;
+            }
+            if(nota == mnota)
+                mres = mres>res?res:mres;
+            //mres = nota>=mnota?(mres>=res?mres:res):mres;
         }
-        printf(" Dia %d\n %d\n\n", ++dia, mres);
+        printf("Dia %d\n %d\n\n", ++dia, mres);
         mnota=0;
-        mres=0;
+        mres=10000001;
+
     }
 
     return 0;
